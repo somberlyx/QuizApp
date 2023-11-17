b76a25924c82fc02c4c58103a2e0ac187388dab4
somberlyx <lybasiddiqui02@gmail.com>
2023-11-17 18:46:39 +0400

    link js file with Q&As stored as objects & arrays

---
+++ quiz.js
@@ -0,0 +1,46 @@
+const questions = [
+  {
+    question: "Which animal is known as the 'Ship of the Desert'?",
+    options: [
+      { choice: "Cow", correct: false },
+      { choice: "Moose", correct: false },
+      { choice: "Camel", correct: true },
+      { choice: "Cactus", correct: false }
+    ]
+  },
+  {
+    question: "Which is the biggest continent in the world?",
+    options: [
+      { choice: "North America", correct: false },
+      { choice: "Asia", correct: true },
+      { choice: "Africa", correct: false },
+      { choice: "Australia", correct: false }
+    ]
+  },
+  { question: "Which is the longest river in the world?",
+    options: [
+      { choice: "Great Ganga", correct: false },
+      { choice: "Nile", correct: true },
+      { choice: "Amazon", correct: false },
+      { choice: "Niger", correct: false }
+    ]
+  },
+  {
+    question: "Which is the largest flower in the world?",
+    options: [
+      { choice: "Rafflesia", correct: true },
+      { choice: "Balloon Flower", correct: false },
+      { choice: "Camellia", correct: false },
+      { choice: "Jasmine", correct: false }
+    ]
+  },
+  {
+    question: "Which is largest island in the world?",
+    options: [
+      { choice: "New Guinea", correct: false },
+      { choice: "Andaman Nicobar", correct: false },
+      { choice: "Hawaii", correct: false },
+      { choice: "Greenland", correct: true }
+    ]
+  }
+]
